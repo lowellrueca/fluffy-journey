75faeada888954ae5c009e4827168daf3e270feb
lowellrueca <lowellrueca@gmail.com>
2022-06-25 06:13:26 +0800

    rename ls_model to pydantic_list_model

--- ext/base.py
+++ ext/base.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, Type
 
 class BaseRepository:
     db_model: Type[Model]
-    ls_model: Type[PydanticListModel]
+    pydantic_list_model: Type[PydanticListModel]
 
     async def get_all(self) -> QuerySet[Model]:
         return self.db_model.all()
@@ -16,7 +16,7 @@ class BaseRepository:
         return self.db_model.all().offset(offset=offset).limit(limit=limit)
 
     async def serialize_query_set(self, query_set: QuerySet[Model]) -> Any:
-        models: PydanticListModel = await self.ls_model.from_queryset(query_set)
+        models: PydanticListModel = await self.pydantic_list_model.from_queryset(query_set)
         content: Dict[str, Any] = {}
         content = models.schema()
         content["data"] = orjson.loads(models.json())
