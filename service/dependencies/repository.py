a6dc878e2911d78f005931e0ec475b5e474040f3
lowellrueca <lowellrueca@gmail.com>
2022-06-25 06:33:10 +0800

    add import for steel model to add attribute pydantic_model attr on SteelRepository class

--- service/dependencies/repository.py
+++ service/dependencies/repository.py
@@ -1,8 +1,9 @@
 from ext import BaseRepository
-from service.models import Steel, SteelModelQuerySet
+from service.models import Steel, SteelModel, SteelModelQuerySet
 from typing import Type
 
 
 class SteelRepository(BaseRepository):
     db_model: Type[Steel] = Steel
     pydantic_list_model: Type[SteelModelQuerySet] = SteelModelQuerySet
+    pydantic_model: Type[SteelModel] = SteelModel
