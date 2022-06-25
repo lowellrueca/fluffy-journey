5d8241a8aee5aedea1de3769e8c414e6fbe29596
lowellrueca <lowellrueca@gmail.com>
2022-06-25 10:22:02 +0800

    creating steel dto

--- service/models/dto.py
+++ service/models/dto.py
@@ -1,10 +1,18 @@
 """ Contains data pydantic models """
 
 from service.models import Steel
+from starlite import DTOFactory
 from tortoise.contrib.pydantic.base import PydanticListModel, PydanticModel
 from tortoise.contrib.pydantic.creator import pydantic_model_creator, pydantic_queryset_creator
 from typing import Type
 
 
+dto_factory: DTOFactory = DTOFactory()
+
 SteelModel: Type[PydanticModel] = pydantic_model_creator(Steel)
 SteelModelQuerySet: Type[PydanticListModel] = pydantic_queryset_creator(Steel)
+SteelDTO = dto_factory(
+    name="SteelDTO", 
+    source=SteelModel, 
+    field_mapping={"id": "id", "name":"name"}
+)
