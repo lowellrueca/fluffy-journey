43033aa184c1a194004ec0b4983e912759ef7151
lowellrueca <lowellrueca@gmail.com>
2022-06-25 23:20:42 +0800

    omit old ProductController class

--- service/app.py
+++ service/app.py
@@ -6,11 +6,9 @@ from typing import List
 
 def startup():
     from service.events import on_startup, on_shutdown
-    from service.controllers import ProductController
 
     on_startup = [on_startup]
     on_shutdown = [on_shutdown]
-    route_handlers: List[ControllerRouterHandler] = [ProductController]
 
     app: App = App(
         route_handlers=route_handlers, 
