bc9bf5e3860e60b5443a4a7fee858bdbd30cb0e5
lowellrueca <lowellrueca@gmail.com>
2022-06-25 22:12:39 +0800

    importing SteelDTO class in model __init__

--- service/models/__init__.py
+++ service/models/__init__.py
@@ -1,6 +1,6 @@
 from .db import Steel
-from .dto import SteelModel, SteelModelQuerySet
+from .dto import SteelModel, SteelModelQuerySet, SteelDTO
 from .data import seed_data
 
 
-__all__ = ["Steel", "SteelModel", "SteelModelQuerySet", "seed_data"]
+__all__ = ["Steel", "SteelModel", "SteelModelQuerySet", "SteelDTO", "seed_data"]
