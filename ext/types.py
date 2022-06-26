af8fcc0086fdc06bda35b58a14e8590b016c13eb
lowellrueca <lowellrueca@gmail.com>
2022-06-26 13:12:12 +0800

    remove bound for steel model and imports

--- ext/types.py
+++ ext/types.py
@@ -1,6 +1,5 @@
-from service.models import Steel
 from tortoise.models import Model
 from typing import  TypeVar
 
 
-TModel = TypeVar("TModel", bound=Model | Steel)
+TModel = TypeVar("TModel", bound=Model)
